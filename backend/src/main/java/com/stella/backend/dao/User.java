a7c9f5640c7f11e5882f27abb7dd18f6c24432f9
LiyanageRS <IT22366368@my.sliit.lk>
2024-10-10 10:38:51 +0530

    Backend
    All routes

---
+++ backend/src/main/java/com/stella/backend/dao/User.java
@@ -0,0 +1,79 @@
+package com.stella.backend.dao;
+import com.stella.backend.config.Role;
+import com.stella.backend.model.WasteEntry;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.Set;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "t_user")
+public class User implements UserDetails {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer user_id;
+    @Column(nullable = false)
+    private String first_name;
+    @Column(nullable = false)
+    private String last_name;
+    @Column(unique = true, nullable = false)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    @Column(nullable = false)
+    private String age;
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private String gender;
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(role.name()));
+    }
+    @OneToMany(mappedBy = "user")
+    private Set<WasteEntry> wasteEntries;
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+
+
+}
+
+
